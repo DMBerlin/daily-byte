462315dc1ee9c7f62bc137a8f049f35286e5a0d3
Daniel Marinho <contato@danielmarinho.dev>
2023-03-28 22:44:01 -0300

    chore: add time complexity

--- intersection-of-numbers.js
+++ intersection-of-numbers.js
@@ -13,7 +13,9 @@
 function intersects (nums1, nums2) {
   let set = new Set();
 
+  // O(n)
   for (let n of nums2) {
+    // O(m)
     if (nums1.some((num) => num === n)) {
       set.add(n);
     }
@@ -27,3 +29,5 @@ function intersects (nums1, nums2) {
   { nums1: [1, 2, 3, 3], nums2: [3, 3] },       // returns [3]
   { nums1: [2, 4, 6, 8], nums2: [1, 3, 5, 7] }, // returns []
 ].forEach((nums) => console.log(nums, intersects(nums.nums1, nums.nums2)));
+
+// Time: O(n * m) => O(n) <constant time>
