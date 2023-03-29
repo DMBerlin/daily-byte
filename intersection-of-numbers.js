ae30437c40b14845aaea76801a12f337d85fd5ca
Daniel Marinho <contato@danielmarinho.dev>
2023-03-28 22:43:39 -0300

    chore: intersection detection using set

---
+++ intersection-of-numbers.js
@@ -0,0 +1,29 @@
+/**
+ * This question is asked by Google. Given two integer arrays, return their intersection.
+ * Note: the intersection is the set of elements that are common to both arrays.
+ * 
+ * Ex: Given the following arrays...
+ * 
+ * nums1 = [2, 4, 4, 2], nums2 = [2, 4], return [2, 4]
+ * nums1 = [1, 2, 3, 3], nums2 = [3, 3], return [3]
+ * nums1 = [2, 4, 6, 8], nums2 = [1, 3, 5, 7], return []
+ * 
+ */
+
+function intersects (nums1, nums2) {
+  let set = new Set();
+
+  for (let n of nums2) {
+    if (nums1.some((num) => num === n)) {
+      set.add(n);
+    }
+  }
+
+  return [...set];
+}
+
+[
+  { nums1: [2, 4, 4, 2], nums2: [2, 4] },       // returns [2, 4]
+  { nums1: [1, 2, 3, 3], nums2: [3, 3] },       // returns [3]
+  { nums1: [2, 4, 6, 8], nums2: [1, 3, 5, 7] }, // returns []
+].forEach((nums) => console.log(nums, intersects(nums.nums1, nums.nums2)));
