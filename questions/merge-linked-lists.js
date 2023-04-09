147248ad1c3a4ad0d6f964567da2d12575606d9d
Daniel Marinho <contato@danielmarinho.dev>
2023-04-09 01:05:55 -0300

    chore: mount base for merge question [wip]

---
+++ questions/merge-linked-lists.js
@@ -0,0 +1,37 @@
+/**
+ * This question is asked by Apple.
+ * 
+ * Given two sorted linked lists, merge them together in ascending order
+ * and return a reference to the merged list.
+ * 
+ * Ex: Given the following lists...
+ * 
+ * list1 = 1->2->3, list2 = 4->5->6->null, return 1->2->3->4->5->6->null
+ * list1 = 1->3->5, list2 = 2->4->6->null, return 1->2->3->4->5->6->null
+ * list1 = 4->4->7, list2 = 1->5->6->null, return 1->4->4->5->6->7->null
+ * 
+ */
+
+const LinkedList = require('./utils/linked-list/list');
+
+/**
+ * @param {LinkedList} a
+ * @param {LinkedList} b
+ */
+function merge (a, b) {
+  a.printAll();
+  b.printAll();
+}
+
+[
+  { a: [1, 2, 3], b: [4, 5, 6] },
+  { a: [1, 3, 5], b: [2, 4, 6] },
+  { a: [4, 4, 7], b: [1, 5, 6] },
+].forEach((test) => 
+  console.log(
+    merge(
+      LinkedList.createFromArray(test.a),
+      LinkedList.createFromArray(test.b)
+    )
+  )
+);
