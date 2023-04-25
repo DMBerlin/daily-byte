fdd77d20c3fffd7686c90413b2c5da60efae7f70
Daniel Marinho <contato@danielmarinho.dev>
2023-04-24 23:11:33 -0300

    chore: contain cycle

---
+++ questions/contains-cycle.js
@@ -0,0 +1,39 @@
+/**
+ * This question is asked by Microsoft.
+ * 
+ * Given a linked list, containing unique numbers, return whether or not it has a cycle.
+ * Note: a cycle is a circular arrangement (i.e. one node points back to a previous node)
+ * 
+ * Ex: Given the following linked lists...
+ * 
+ * 1->2->3->1 -> true (3 points back to 1)
+ * 1->2->3 -> false
+ * 1->1 true (1 points to itself)
+ * 
+ */
+
+const LinkedList = require('../questions/utils/linked-list/list.class');
+
+function containsCycle (head) {
+  let node = head;
+  let visited = new Map();
+  // O(n)
+  while (node) {
+      // O(1)
+      let ref = visited.get(node);
+      if (ref && ref === node) {
+          return true;
+      } else {
+          // O(1)
+          visited.set(node, node);
+      }
+      node = node.next;
+  }
+  return false;
+}
+
+[
+  [LinkedList.createFromArray([1, 2, 3], { circular: true }), true],
+  [LinkedList.createFromArray([4, 5, 6]), false],
+  [LinkedList.createFromArray([7], { circular: true }), true]
+].forEach(([actual, expected]) => console.log(containsCycle(actual) === expected ? 'pass' : 'failed'));
