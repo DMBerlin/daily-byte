10529b124f31f11eb09e1287f78024e3c8ba32d7
Daniel Marinho <contato@danielmarinho.dev>
2023-04-25 22:21:13 -0300

    chore: valid brackets

---
+++ questions/valid-characters.js
@@ -0,0 +1,39 @@
+/**
+ * This question is asked by Google.
+ * 
+ * Given a string only containing the following characters (, ), {, }, [, and ]
+ * return whether or not the opening and closing characters are in a valid order.
+ * 
+ * Ex: Given the following strings...
+ * 
+ * "(){}[]", return true
+ * "(({[]}))", return true
+ * "{(})", return false
+ * 
+ */
+
+function validCharacters (brackets) {
+  const closingBracketFor = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  
+  const openingBrackets = [];
+  
+  for (const bracket of brackets) {
+    if (bracket in closingBracketFor) {
+      openingBrackets.push(bracket);
+    } else if (closingBracketFor[openingBrackets.pop()] !== bracket) {
+      return false;
+    }
+  }
+  
+  return openingBrackets.length === 0;
+}
+
+[
+  [['(', ')', '{', '}', '[', ']'], true],
+  [['(', '(', '{', '[', ']', '}', ')', ')'], true],
+  [['{', '(', '}', ')'], false],
+].forEach(([actual, expected]) => console.log(validCharacters(actual) === expected ? 'pass' : 'fail'));
