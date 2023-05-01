eb627025cd0e322d0934a5172abeebc8eafdd793
Daniel Marinho <contato@danielmarinho.dev>
2023-05-01 18:12:11 -0300

    create: match array

--- utils/expect/a-to-b.expect.js
+++ utils/expect/a-to-b.expect.js
@@ -2,5 +2,13 @@
 module.exports = function expect(actual) {
   return {
     toBe: (expected) => console.log(actual === expected ? 'pass' : 'fail'),
+    toMatchArray: (expected) => {
+      for (let n = 0; n < actual.length; n++) {
+        if (actual[n] !== expected[n]) {
+          return console.log('fail', { expected, received: actual });
+        }
+      }
+      return console.log('pass');
+    },
   }
 }
